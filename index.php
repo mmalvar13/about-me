068547056d2916900ac64ecfc84bb534665eaa1c
Monica Alvarez <mmalvar13@gmail.com>
2016-07-12 16:50:39 -0600

    created html skeleton

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8"/>
+		<title>About Me-Monica M. Alvarez</title>
+	</head>
+	<body>
+		<header>
+			<h1>Monica M. Alvarez</h1>
+		</header>
+		<main>
+			<p>
+				
+			</p>
+		</main>
+		<
+
+	</body>
+</html>
