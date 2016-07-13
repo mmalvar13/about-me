665c969379b68d0d658326462fc553f9e8f6c064
Monica Alvarez <mmalvar13@gmail.com>
2016-07-12 23:40:09 -0600

    added css stylesheet details

--- index.php
+++ index.php
@@ -8,16 +8,37 @@
 	<body>
 		<header>
 			<h1>Monica M. Alvarez</h1>
+			<img src="images/Me.png" alt="Monica Alvarez"/>
+			<p>Purr for no reason eat from dog's food or scream at teh bath play time. This human feeds me, i should be a god russian blue, and kick up litter. Meow all night having their mate disturbing sleeping humans poop in litter box, scratch the walls, so refuse to drink water except out of someone's glass, and walk on car leaving trail of paw prints on hood and windshield, eat prawns daintily with a claw then lick paws clean wash down prawns with a lap of carnation milk then retire to the warmest spot on the couch to claw at the fabric before taking a catnap. Favor packaging over toy lay on arms while you're using the keyboard sit by the fire so give attitude, or favor packaging over toy use lap as chair. Ears back wide eyed give attitude swat at dog, and gnaw the corn cob attack the dog then pretend like nothing happened</p>
 		</header>
 		<main>
-			<img src="https://scontent-lax3-1.xx.fbcdn.net/v/t1.0-9/644731_10200473050740227_1204312920_n.jpg?oh=c992963fd9204b9c073c1a8d96a44874&oe=57EF8A2F"/>
+
 			<img src="https://scontent-lax3-1.xx.fbcdn.net/v/t1.0-9/13133234_1012357712133616_1951519903953552062_n.jpg?oh=657921ce238cb06e2280d45f6d70ea86&oe=57EA9602"/>
 			<p>
 				Human give me attention meow paw at your fat belly yet rub face on everything stares at human while pushing stuff off a table slap owner's face at 5am until human fills food dish or attack dog, run away and pretend to be victim. Unwrap toilet paper scamper for always hungry so find empty spot in cupboard and sleep all day burrow under covers. Loves cheeseburgers ste
 			</p>
 			<a href="https://reddit.com">My Favorite Website</a>
 
-			<table>
+			<table border="3px">
+				<thead>
+					<tr>
+						<th colspan="2">Facts About Me</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<th> Age</th>
+						<td>25</td>
+					</tr>
+					<tr>
+						<th> Height</th>
+						<td>5'2"</td>
+					</tr>
+					<tr>
+						<th>Sign</th>
+						<td>Aries</td>
+					</tr>
+				</tbody>
 
 			</table>
 		</main>
