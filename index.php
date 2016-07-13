05054f97c299e56f652cef0b28557e8bc7981995
Monica Alvarez <mmalvar13@gmail.com>
2016-07-13 08:29:24 -0600

    added intro photo and description into a table side by side

--- index.php
+++ index.php
@@ -2,22 +2,45 @@
 <html lang="en">
 	<head>
 		<meta charset="UTF-8"/>
+		<table>
+			<tr>
+				<td>My Favorite Website</td>
+				<td>    </td>
+				<td>    </td>
+				<td>My Personal Website</td>
+				<td> </td>
+				<td>Portfolio</td>
+			</tr>
+
+		</table>
+
 		<title>About Me-Monica M. Alvarez</title>
 		<link href="style.css" rel="stylesheet" type="text/css"/>
 	</head>
 	<body>
 		<header>
-			<h1>Monica M. Alvarez</h1>
-			<img src="images/Me.png" alt="Monica Alvarez"/>
-			<p>Purr for no reason eat from dog's food or scream at teh bath play time. This human feeds me, i should be a god russian blue, and kick up litter. Meow all night having their mate disturbing sleeping humans poop in litter box, scratch the walls, so refuse to drink water except out of someone's glass, and walk on car leaving trail of paw prints on hood and windshield, eat prawns daintily with a claw then lick paws clean wash down prawns with a lap of carnation milk then retire to the warmest spot on the couch to claw at the fabric before taking a catnap. Favor packaging over toy lay on arms while you're using the keyboard sit by the fire so give attitude, or favor packaging over toy use lap as chair. Ears back wide eyed give attitude swat at dog, and gnaw the corn cob attack the dog then pretend like nothing happened</p>
+			<table>
+				<<h1>Monica M. Alvarez</h1>
+				<tr>
+					<td><img src="images/Me.png" alt="Monica Alvarez"/></td>
+					<td><p>Purr for no reason eat from dog's food or scream at teh bath play time. This human feeds me, i should be a god russian blue, and kick up litter. Meow all night having their mate disturbing sleeping humans poop in litter box, scratch the walls, so refuse to drink water except out of someone's glass, and walk on car leaving trail of paw prints on hood and windshield, eat prawns daintily with a claw then lick paws clean wash down prawns with a lap of carnation milk then retire to the warmest spot on the couch to claw at the fabric before taking a catnap. Favor packaging over toy lay on arms while you're using the keyboard sit by the fire so give attitude, or favor packaging over toy use lap as chair. Ears back wide eyed give attitude swat at dog, and gnaw the corn cob attack the dog then pretend like nothing happened</p>
+					</td>
+				</tr>
+				<tr>
+					<td> </td>
+					<td>t flee in terror at cucumber discovered on floor. Knock dish off table head butt cant eat out of my own dish pee in the shoe. Jump off balcony, onto stranger's head groom yourself 4 hours - checked, have your beauty sleep 18 hours - checked, be fabulous for the rest of the day - checked!. Destroy couch as revenge. Meowing non stop for food lick the plastic bag and na</td>
+				</tr>
+
+			</table>
 		</header>
 		<main>
+			<h2>My Family</h2>
 
 			<img src="https://scontent-lax3-1.xx.fbcdn.net/v/t1.0-9/13133234_1012357712133616_1951519903953552062_n.jpg?oh=657921ce238cb06e2280d45f6d70ea86&oe=57EA9602"/>
 			<p>
 				Human give me attention meow paw at your fat belly yet rub face on everything stares at human while pushing stuff off a table slap owner's face at 5am until human fills food dish or attack dog, run away and pretend to be victim. Unwrap toilet paper scamper for always hungry so find empty spot in cupboard and sleep all day burrow under covers. Loves cheeseburgers ste
 			</p>
-			<a href="https://reddit.com">My Favorite Website</a>
+			<a href="https://reddit.com" target="_blank">My Favorite Website</a>
 
 			<table border="3px">
 				<thead>
