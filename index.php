7cd612d764390aef5a31c7b20bc6877d8d01d46a
Monica Alvarez <mmalvar13@gmail.com>
2016-07-12 22:07:31 -0600

    added css stylesheet with text color

--- index.php
+++ index.php
@@ -3,15 +3,23 @@
 	<head>
 		<meta charset="UTF-8"/>
 		<title>About Me-Monica M. Alvarez</title>
+		<link href="style.css" rel="stylesheet" type="text/css"/>
 	</head>
 	<body>
 		<header>
 			<h1>Monica M. Alvarez</h1>
 		</header>
 		<main>
+			<img src="https://scontent-lax3-1.xx.fbcdn.net/v/t1.0-9/644731_10200473050740227_1204312920_n.jpg?oh=c992963fd9204b9c073c1a8d96a44874&oe=57EF8A2F"/>
+			<img src="https://scontent-lax3-1.xx.fbcdn.net/v/t1.0-9/13133234_1012357712133616_1951519903953552062_n.jpg?oh=657921ce238cb06e2280d45f6d70ea86&oe=57EA9602"/>
 			<p>
-				
+				Human give me attention meow paw at your fat belly yet rub face on everything stares at human while pushing stuff off a table slap owner's face at 5am until human fills food dish or attack dog, run away and pretend to be victim. Unwrap toilet paper scamper for always hungry so find empty spot in cupboard and sleep all day burrow under covers. Loves cheeseburgers ste
 			</p>
+			<a href="https://reddit.com">My Favorite Website</a>
+
+			<table>
+
+			</table>
 		</main>
 		<
 
