3cc0bc32de2aeca492ff9eb42d411dfa29a16400
Monica Alvarez <mmalvar13@gmail.com>
2016-07-14 21:13:21 -0600

    added photo as page dividers

--- index.php
+++ index.php
@@ -14,18 +14,27 @@
 			<h2>My Favorite Website</h2>
 			<h2>My Portfolio</h2>
 		</header>
-		
+		<!--make it responsive-->
+		<!--add javascript and content-->
+		<!--background photo?-->
 		<div>
 			<img src="images/Me.png" alt="Monica Alvarez"/>
 			<p>Purr for no reason eat from dog's food or scream at teh bath play time. This human feeds me, i should be a god russian blue, and kick up litter. Meow all night having their mate disturbing sleeping humans poop in litter box, scratch the walls, so refuse to drink water except out of someone's glass, and walk on car leaving trail of paw prints on hood and windshield, eat prawns daintily with a claw then lick paws clean wash down prawns with a lap of carnation milk then retire to the warmest spot on the couch to claw at the fabric before taking a catnap. Favor packaging over toy lay on arms while you're using the keyboard sit by the fire so give attitude, or favor packaging over toy use lap as chair. Ears back wide eyed give attitude swat at dog, and gnaw the corn cob attack the dog then pretend like nothing happened</p>
 		</div>
 		
 		
+		
 		<main>
+			<div><h3>My Family</h3></div>
+			<img src="images/family.png"/>
+			<img src="images/christmasInPlaza.png"/>
+			<img src="images/MeAndAlex.png"/>
+			<img src="images/Daddio.png"/>
 
-			<h3>My Family</h3>
-			<img src="https://scontent-lax3-1.xx.fbcdn.net/v/t1.0-9/13133234_1012357712133616_1951519903953552062_n.jpg?oh=657921ce238cb06e2280d45f6d70ea86&oe=57EA9602"/>
 			<a href="https://reddit.com" target="_blank">My Favorite Website</a>
+
+			<h4>My Hobbies</h4>
+			
 		</main>
 		<
 
